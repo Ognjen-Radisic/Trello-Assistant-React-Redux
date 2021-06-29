83f8d190f1097e7792503c7a25d000e4d983bba3
Ognjen <radisicognjen@gmail.com>
2021-06-29 15:59:38 +0200

    style update and delete card visual

--- src/components/board/SingleCard/index.js
+++ src/components/board/SingleCard/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FiTrash2 } from "react-icons/fi";
+
+//redux
+import { useDispatch } from "react-redux";
+import { deleteCard } from "../../../actions/cards";
 
 const SingleCard = ({ id, name }) => {
+	const deleteCard = () => {};
 	return (
 		<div className="single-card">
 			<p>{name}</p>
+			<FiTrash2 className="single-card__delete-icon" onClick={deleteCard} />
 		</div>
 	);
 };
