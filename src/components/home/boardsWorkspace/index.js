1de0f3660b471baa16b966a81bae98637d2fc23f
Ognjen <radisicognjen@gmail.com>
2021-06-30 07:56:27 +0200

    dynamic userName

--- src/components/home/boardsWorkspace/index.js
+++ src/components/home/boardsWorkspace/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import BoardCard from "../BoardCard";
 import CreateBoardCard from "../CreateBoardCard";
 
@@ -6,17 +6,40 @@ import CreateBoardCard from "../CreateBoardCard";
 import { useDispatch, useSelector } from "react-redux";
 import { getBoards } from "../../../actions/boards";
 
+//for fetching user name user name
+import trello from "../../../trelloData";
+const url = "https://api.trello.com/1/members/me?";
+
 const BoardsWorkspace = () => {
 	const dispatch = useDispatch();
 	const boards = useSelector((state) => state.boards);
+	const [userName, setUserName] = useState("");
 
 	useEffect(() => {
 		dispatch(getBoards());
 	}, [dispatch]);
 
+	useEffect(() => {
+		const fetchUsersName = async () => {
+			try {
+				const res = await fetch(
+					`${url}key=${trello.key}&token=${trello.token}`
+				);
+				const data = await res.json();
+
+				if (data) {
+					setUserName(data.fullName);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchUsersName();
+	}, []);
+
 	return (
 		<div className="board-workspace">
-			<h3 className="board-workspace__title">Ognjen Radisic's workspace</h3>
+			<h3 className="board-workspace__title">{userName}'s workspace</h3>
 			<div className="board-workspace__container">
 				<CreateBoardCard howManyBoards={boards.length} />
 				{boards.map((item) => {
