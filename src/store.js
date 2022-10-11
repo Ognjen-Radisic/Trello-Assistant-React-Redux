32b4f2abb0a94dd7c44a78d321ca0849c36b96e6
Ognjen <radisicognjen@gmail.com>
2022-10-12 01:34:29 +0200

    Fix when app runned localy without redux dev tools installed, app crashes

--- src/store.js
+++ src/store.js
@@ -2,12 +2,17 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 
-const store = createStore(
-	reducers,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+const composeEnhancers =
+	typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+				// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+		  })
+		: compose;
+
+const enhancer = composeEnhancers(
+	applyMiddleware(thunk)
+	// other store enhancers if any
 );
+const store = createStore(reducers, enhancer);
 
 export default store;
