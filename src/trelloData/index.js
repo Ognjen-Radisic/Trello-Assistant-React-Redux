ff653201501b95a4568822762ae52eb774587df8
Ognjen <radisicognjen@gmail.com>
2022-10-12 23:23:51 +0200

    Add env-variables to the projects and enc-cmd library to run it localy

--- src/trelloData/index.js
+++ src/trelloData/index.js
@@ -1,6 +1,6 @@
 const trello = {
-	key: "aac63253c1f0e7b35d16e559b6d2aa73",
-	token: "5dd3b367a304d073a517660238622708da2df479a6bbdcd5bc6430661a81db96",
+	key: process.env.REACT_APP_TRELLO_KEY,
+	token: process.env.REACT_APP_TRELLO_TOKEN,
 };
 
 export default trello;
